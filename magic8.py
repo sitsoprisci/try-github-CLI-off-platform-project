e24ced50e2711137a00e88a612472dc7ff173a79
Priscilla Sitsofe Agboado <sitsoprisy4@gmail.com>
2022-05-31 16:52:24 +0000

    Fixed magic8.py file by importing the proper required library

--- magic8.py
+++ magic8.py
@@ -1,9 +1,10 @@
+import random
 name = "Joe"
 question = "Will I win the lottery?"
 answer = ""
 
 random_number = random.randint(1, 9)
-# print(random_number)
+  # print(random_number)
 
 if random_number == 1:
   answer = "Yes - definitely"
